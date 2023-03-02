2aa6fc17daf75def1f6221ae88ec4d45f11ee684
ehuntober <ehuntercrypto@gmail.com>
2023-03-02 13:32:36 +0000

    view restframe api_view added

--- drinks/views.py
+++ drinks/views.py
@@ -1,10 +1,11 @@
 from django.http import JsonResponse
 from .models import Drink 
 from .serializers import DrinkSerializer
+from rest_framework.decorators import api_view
 
+@api_view('GET','POST')
 def drink_list(request):
-    
     drinks = Drink.objects.all()
     serializer =DrinkSerializer(drinks,many=True)
-    return JsonResponse(serializer.data)
+    return JsonResponse({"drinks": serializer.data},safe=False)
     
